fb1619605f79ae08ab51bc4b020dfad9aaa8e083
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2023-03-19 15:01:53 -0300

    Update routes and create new pages

---
+++ src/pages/notFound/index.tsx
@@ -0,0 +1,7 @@
+export const NotFound = () => {
+  return (
+    <div>
+      <h1>não encontrada</h1>
+    </div>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ import { Notes } from '../pages/notes';
 import { createBrowserRouter } from "react-router-dom";
 import { Signup } from '../pages/signup';
 import { Login } from '../pages/login';
+import { NotFound } from '../pages/notFound';
+import { LandingPage } from '../pages/landingPage';
 
 export const routes = createBrowserRouter([
   {
@@ -11,10 +13,13 @@ export const routes = createBrowserRouter([
     path: '/auth/signup',
     element: <Signup />,
   }, {
-    path: '/notes',
+    path: '/app/notes',
     element: <Notes />,
   }, {
+    path: '/',
+    element: <LandingPage />
+  }, {
     path: '*',
-    element: <Notes />,
+    element: <NotFound />,
   }
 ])
