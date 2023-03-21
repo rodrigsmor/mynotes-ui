dacd2d6835a8098b79a0d68a678a9ac47743e86c
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2023-03-20 23:21:26 -0300

    Develops the NotFound page

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,7 +1,22 @@
+import { Button } from '../../components/buttons/Button';
+import { ThemeEnums } from '../../utils/enums/ThemeEnums';
+import { DetailedNotFoundMessage, GlitchTextEffect, NotFoundIllustration, NotFoundMessage, NotFoundPageContainer } from './styled';
+import LookingForNotFoundPage_Illustration from '../../assets/images/illustrations/LookingForNotFoundPage_Illustration.svg';
+
 export const NotFound = () => {
   return (
-    <div>
-      <h1>não encontrada</h1>
-    </div>
+    <NotFoundPageContainer>
+      <GlitchTextEffect data-text='404'>404</GlitchTextEffect>
+      <NotFoundMessage>Página não encontrada</NotFoundMessage>
+      <DetailedNotFoundMessage>
+        Parece que a página que você está procurando não existe. Verifique se a URL informada está correta ou tente voltar para a página inicial.
+      </DetailedNotFoundMessage>
+      <Button theme={ThemeEnums.DEFAULT} to={'/'}>
+        <>voltar para a página inicial</>
+      </Button>
+      <NotFoundIllustration
+        src={LookingForNotFoundPage_Illustration}
+      />
+    </NotFoundPageContainer>
   );
 }
