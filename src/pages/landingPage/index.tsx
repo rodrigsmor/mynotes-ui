2d85376694878237a223d62c9efb8c6a5ae15026
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2023-03-23 22:37:24 -0300

    Develops Home section in landing page

--- src/pages/landingPage/index.tsx
+++ src/pages/landingPage/index.tsx
@@ -1,6 +1,9 @@
+import { Button } from '../../components/buttons/Button';
 import { Header } from '../../components/layouts/Header';
-import { LandingPageContainer, HomeSectionLP } from './styled';
+import { LandingPageContainer, HomeSectionLP, HomeArrowAnchor } from './styled';
 import { useRef, useState } from 'react';
+import { ThemeEnums } from '../../utils/enums/ThemeEnums';
+import { HiOutlineChevronDoubleDown } from 'react-icons/hi2';
 
 export const LandingPage = () => {
   const ref = useRef<HTMLDivElement>();
@@ -16,10 +19,21 @@ export const LandingPage = () => {
       }}
     >
       <Header isLandingPage isLogged={false} isScrolled={hasUserScrolled} />
-      <HomeSectionLP>
+      <HomeSectionLP id='home'>
         <small>chega de preocupações</small>
         <h1>Tenha controle sobre suas <span>anotações</span>!</h1>
         <p>Se você não vive sem um papel e caneta no seu cotidiano, o MyNotes irá tornar sua vida mais organizada. Diga adeus a pilhas enormes de bloco de notas, abrace o <strong>MyNotes</strong>!</p>
+        <div className='landingPage-Home_ButtonGroup'>
+          <Button theme={ThemeEnums.DEFAULT} to='/auth/signup'>
+            <>começar agora</>
+          </Button>
+          <Button theme={ThemeEnums.SURFACE} to='/#about-project'>
+            <>saber mais</>
+          </Button>
+        </div>
+        <HomeArrowAnchor to='/#about-project'>
+          <HiOutlineChevronDoubleDown size={44} />
+        </HomeArrowAnchor>
       </HomeSectionLP>
     </LandingPageContainer>
   );
--- src/pages/landingPage/styled.ts
+++ src/pages/landingPage/styled.ts
@@ -1,17 +1,18 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const LandingPageContainer = styled.div`
   width: 100vw;
-  height: 100vh;
   max-width: 100vw;
   overflow-y: auto;
-  background: ${({ theme }) => theme.colors.background.light };
+  height: fit-content;
+  /* background: ${({ theme }) => theme.colors.background.light }; */
 `;
 
 export const HomeSectionLP = styled.div`
   width: 100vw;
-  height: 100vh;
   display: flex;
+  height: 100dvh;
   padding: 0 16px;
   align-items: center;
   flex-direction: column;
@@ -63,6 +64,18 @@ export const HomeSectionLP = styled.div`
     }
   }
 
+  > .landingPage-Home_ButtonGroup {
+    gap: 16px;
+    width: 100%;
+    display: grid;
+    min-width: 100%;
+    height: fit-content;
+    align-items: center;
+    grid-auto-flow: row;
+    justify-content: stretch;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
   @media ${({ theme }) => theme.media.desktop } {
     padding: 0 15.12%;
 
@@ -77,5 +90,23 @@ export const HomeSectionLP = styled.div`
       margin: 18px 0 36px 0;
       font-size: ${({ theme }) => theme.typography.size.h4 };
     }
+
+    > .landingPage-Home_ButtonGroup {
+      width: 37.2vw;
+      min-width: 37.2vw;
+    }
+  }
+`;
+
+export const HomeArrowAnchor = styled(Link)`
+  bottom: 24px;
+  height: 44px;
+  position: absolute;
+  transition: ease .5s;
+  color: ${({ theme }) => theme.colors.subtext.light };
+
+  &:hover {
+    margin: 0 0 20px 0;
+    transform: scale(1.5);
   }
 `;
