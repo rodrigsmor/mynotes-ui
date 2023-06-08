a7fd3c06a34705893cfc0bb1ab4a385cbda1891f
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-06-07 23:29:21 -0300

    :sparkles: feat: adds click outside close of user dropdown

--- src/components/cards/LoggedAccountCard/index.tsx
+++ src/components/cards/LoggedAccountCard/index.tsx
@@ -1,14 +1,18 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Button } from '../../buttons/Button';
 import { IconButton } from '../../buttons/IconButton';
 import { connectedAccounts } from '../../../utils/mock';
 import { ThemeEnums } from '../../../utils/enums/ThemeEnums';
 import { HiArrowLeftOnRectangle, HiOutlineEllipsisVertical, HiOutlineUser, HiOutlineUserPlus } from 'react-icons/hi2';
 import { LoggedAccountCardContainer, ProfilePicture, UserName, UserEmail, DropdownNav, DropdownOption, AccountsSection, AccountCardSummary } from './styled';
+import useOutsideClick from '../../../utils/hooks/useOutsideClick';
 
 export const LoggedAccountCard = () => {
+  const dropdownRef = useRef<HTMLDivElement>(null);
   const [ isDropdownOpen, setIsDropdownOpen ] = useState<boolean>(false);
 
+  useOutsideClick(dropdownRef, () => setIsDropdownOpen(false));
+  
   return (
     <LoggedAccountCardContainer className={`logged_account_card`}>
       <ProfilePicture src='https://img.freepik.com/free-photo/shallow-focus-shot-young-black-male-grey-wall_181624-52039.jpg?w=2000' alt='' />
@@ -21,7 +25,7 @@ export const LoggedAccountCard = () => {
         onClick={(e) => setIsDropdownOpen(!isDropdownOpen)} 
         attributes={{ id: 'user-dropdown_button', "aria-label": 'See user settings', "aria-haspopup": true, "aria-expanded": isDropdownOpen, "aria-controls": 'user-dropdown' }}
       />
-      <DropdownNav id='user-dropdown' aria-hidden={!isDropdownOpen} aria-labelledby='user-dropdown_button'>
+      <DropdownNav ref={dropdownRef} id='user-dropdown' aria-hidden={!isDropdownOpen} aria-labelledby='user-dropdown_button'>
         <DropdownOption to='/app/profile' tabIndex={isDropdownOpen ? 0 : -1}>
           <HiOutlineUser size={18} /> Seu perfil
         </DropdownOption>
