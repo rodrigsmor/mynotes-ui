e2f46fd37a2b364cd31b5842e93535b930150c15
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-06-08 20:15:31 -0300

    :wheelchair: fix: allows to select an option via the keyboard

--- src/components/forms/Select/index.tsx
+++ src/components/forms/Select/index.tsx
@@ -1,5 +1,5 @@
-import { useState, MouseEvent, useRef } from 'react';
 import { HiChevronDown } from "react-icons/hi2";
+import { useState, MouseEvent, KeyboardEvent, useRef } from 'react';
 import { SelectInput, SelectOption, SelectOptionsWrapper, SelectWrapper } from "./styled";
 
 type SelectOptionsValues = {
@@ -20,11 +20,17 @@ export const Select: React.FC<SelectProps> = ({ className, options, name, attrib
   const inputRef = useRef<HTMLButtonElement>(null);
   const [ showOptions, setShowOptions ] = useState<boolean>(false)
 
-  const onSelect = (event: MouseEvent, data: string) => {
+  const onSelect = (event: MouseEvent | KeyboardEvent, data: string) => {
     event.preventDefault();
     setShowOptions(false);
     setSelected(selected === data ? '' : data)
     inputRef.current?.focus();
+  }
+
+  const handleClick = (event: KeyboardEvent, data: string) => {
+    if(event.key === 'Escape' || event.key === 'Enter') {
+      onSelect(event, data)
+    }
   }
   
   return (
@@ -39,7 +45,7 @@ export const Select: React.FC<SelectProps> = ({ className, options, name, attrib
       <SelectOptionsWrapper className={`select_form-OptionsList ${showOptions && 'opened'}`} role='listbox' aria-expanded={showOptions}>
         {
           options.map(({ name, value }, id) => (
-            <SelectOption key={id} role='option' tabIndex={0} onClick={e => onSelect(e, value)} className={`${selected === value && 'selected'}`}>
+            <SelectOption key={id} role='option' tabIndex={0} onKeyDown={e => handleClick(e, value)} onClick={e => onSelect(e, value)} className={`${selected === value && 'selected'}`}>
               { value }
             </SelectOption>
           ))
