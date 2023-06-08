21fc23c07707ef344695b1b2c4d65ee79ae2f15e
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-06-07 23:25:29 -0300

    :recycle: refactor: in order to allows also keyboard events

--- src/utils/hooks/useOutsideClick.tsx
+++ src/utils/hooks/useOutsideClick.tsx
@@ -2,21 +2,25 @@ import { useEffect, RefObject } from 'react';
 
 const useOutsideClick = <T extends HTMLElement>(
   ref: RefObject<T>,
-  callback: () => void
+  close: () => void
 ): void => {
-  const handleClick = (event: MouseEvent): void => {
-    if (ref.current && !ref.current.contains(event.target as Node)) {
-      callback();
+  const handleClick = (event: MouseEvent | KeyboardEvent): void => {
+    if (ref.current && !ref.current.contains(event.target as Node) && event instanceof MouseEvent) {
+      close();
+    } else if(event instanceof KeyboardEvent && (event.key === 'Escape' || event.key === 'Enter')) {
+      close()
     }
   };
 
   useEffect(() => {
+    document.addEventListener('keydown', handleClick);
     document.addEventListener('mousedown', handleClick);
 
     return () => {
+      document.removeEventListener('keydown', handleClick);
       document.removeEventListener('mousedown', handleClick);
     };
-  }, [ref, callback]);
+  }, [ref, close]);
 };
 
 export default useOutsideClick;
